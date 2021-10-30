136835e5c1eb2da95a6a3025a8c5883847b5c641
Miguel Machado <93016833+miguelmachadofs@users.noreply.github.com>
2021-10-30 19:13:27 -0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,80 @@
+// comments
+// document.write("Hello")
+
+// Tipos de Dados
+
+// variaveis (serve para guardar alguma coisa pra usar depois)
+// var myvar //undefined
+// var myvar = "He"
+// var ou let
+
+// constants
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso também é uma string'
+// const s3 = `Isso é uma string também`
+
+// documen.write(s1) ou ("Isso é uma string")
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto:possue propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("run Forest")
+//     return "run Forest"
+//   }
+// }
+
+// document.write(pessoa.correr())
+
+// Array - Vetores (coleção de dados)
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// // registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+
+// // executar função
+// sayMyName("Miguel")
+// sayMyName("Mayk")
+// sayMyName("Valeska")
+
+// Condicionais
+
+// const notaFinal = 7
+
+// if( notaFinal < 5 ) {
+//   // caminho 1
+//   document.write("Reprovado")
+// }
+// else {
+//   // caminho 2
+//   document.write("Aprovado")
+// }
+
+// Loop - Repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
